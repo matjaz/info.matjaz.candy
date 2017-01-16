64b642a3f7c74b7b900e9ff8505ae64dd077f4cc
Matjaz Lipus <matjazl@gmail.com>
2017-01-16 22:01:46 +0100

    crashed app trigger

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 'use strict'
 
+const apps = require('./lib/apps')
 const system = require('./lib/system')
 const folderFlows = require('./lib/folderFlows')
 const systemFlows = require('./lib/systemFlows')
 
 exports.init = function () {
+  apps.init()
   system.init()
   folderFlows.init()
   systemFlows.init()
